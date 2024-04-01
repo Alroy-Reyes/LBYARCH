bb7cd7d484c3c1b68d2bfc1ac522b709ec324305
wyz <98019015+altlyza@users.noreply.github.com>
2024-04-02 02:13:22 +0800

    Update asmfunc.asm

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,63 +1,43 @@
-;section .data
-;msg db "Hello World", 13, 10, 0
+section .text
+bits 64
+default rel
 
-;section .text
-;bits 64
-;default rel
+global stencil_asm
 
-;global asmhello
-;extern printf
+stencil_asm:   
+    sub rsp, 8*11       
+    
+    mov r12, rcx ; n
+    mov r13, rdx ; X
+    mov r14, r8  ; Y
+    
+    ; displacement variables
+    mov rbx, 3
+    mov r11, 0
 
-;asmhello:
-;	sub rsp, 8*5
-;	lea rcx, [msg]
-;	call printf
-;	add rsp, 8*5
-;	ret
+LOOP:
+    ; check if all elements of Y are identified
+    cmp r11, r12
+    je end
+    
+    ; stencil operation
+    movsd xmm5, [r13 - 24 + 8*rbx] 
+    addsd xmm5, [r13 - 16 + 8*rbx]
+    addsd xmm5, [r13 - 8 + 8*rbx]
+    addsd xmm5, [r13 + 8*rbx]
+    addsd xmm5, [r13 + 8 + 8*rbx]
+    addsd xmm5, [r13 + 16 + 8*rbx]
+    addsd xmm5, [r13 + 24 + 8*rbx]
+    
+    ; store result in Y
+    movsd [r14 + 8*r11], xmm5
+    
+    inc rbx
+    inc r11
+    jmp LOOP
 
-;section .text
-;bits 64
-;default rel
+end:
+    add rsp, 8*11
+    mov rax, r14 ; return Y
 
-;global sum
-
-;sum:
-;	add rcx, rdx  ; rcx - 1st param, rdx - 2nd param
-;	mov rax, rcx  ; rax - return value
-;	mov qword [r8],1  ; change the “reference” value to 1; r8 is the 3rd param
-;	ret
-
-
-; Assembly implementation of stencil function
-; Inputs:
-;   rdi: n (length of the vectors)
-;   rsi: pointer to X array
-;   rdx: pointer to Y array
-stencil_asm:
-    mov rax, rsi            ; Copy pointer to X array to rax
-    mov rbx, rdx            ; Copy pointer to Y array to rbx
-    mov ecx, 3              ; Initialize loop counter to 3
-
-loop_start:
-    movsd xmm0, [rax - 24]  ; Load X[i - 3] into xmm0
-    movsd xmm1, [rax - 16]  ; Load X[i - 2] into xmm1
-    movsd xmm2, [rax - 8]   ; Load X[i - 1] into xmm2
-    movsd xmm3, [rax]       ; Load X[i] into xmm3
-    movsd xmm4, [rax + 8]   ; Load X[i + 1] into xmm4
-    movsd xmm5, [rax + 16]  ; Load X[i + 2] into xmm5
-    movsd xmm6, [rax + 24]  ; Load X[i + 3] into xmm6
-
-    ; Add all loaded values and store the result in Y[i]
-    addsd xmm3, xmm0
-    addsd xmm3, xmm1
-    addsd xmm3, xmm2
-    addsd xmm3, xmm4
-    addsd xmm3, xmm5
-    addsd xmm3, xmm6
-    movsd [rbx + rcx*8], xmm3  ; Store the result in Y[i]
-
-    add rcx, 1              ; Increment loop counter
-    cmp rcx, rdi            ; Compare loop counter with n
-    jl loop_start           ; If loop counter < n, repeat loop
-
-    ret                     ; Return
+    ret       
